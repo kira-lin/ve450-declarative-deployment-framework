ac3aff8a5f709eb06d9910e0cb01130977c8ee5e
linzhi <linzhilynn@gmail.com>
2018-10-27 07:38:19 +0000

    add serving contianer

---
+++ dags/demo.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import print_function
+import airflow
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.models import DAG
+import os
+
+args = {
+    'owner': 'airflow',
+    'start_date': airflow.utils.dates.days_ago(2)
+}
+
+dag = DAG(
+    dag_id='demo_tensorflow_mnist', default_args=args,
+    schedule_interval=None
+)
+
+
+def run():
+    execfile('/root/airflow/runtime/mnist_keras.py')
+
+# But you can if you want to
+t1 = PythonOperator(
+    task_id="train_and_save", python_callable=run, dag=dag,
+    executor_config={"KubernetesExecutor": {"image": "tensorflow:airflow"}}
+)
+
+serve = '''tensorflow_model_server --port=8500 --rest_api_port=8501 \
+  --model_name=MnistModel --model_base_path=/root/airflow/runtime/models/MnistModel'''
+
+t2 = BashOperator(
+    task_id="serve_model", bash_command=serve, dag=dag,
+    executor_config={"KubernetesExecutor": {"image": "tfserving:airflow"}
+)
---
+++ mnist_keras.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
+tf.keras.backend.set_learning_phase(0)
+export_path = '/root/airflow/runtime/models/MnistModel/1'
+
+# Fetch the Keras session and save the model
+# The signature definition is defined by the input and output tensors
+# And stored with the default serving key
+with tf.keras.backend.get_session() as sess:
+    tf.saved_model.simple_save(
+        sess,
+        export_path,
+        inputs={'input_image': tf.convert_to_tensor(x_train)},
+        outputs={"digit": tf.convert_to_tensor(y_train)})
